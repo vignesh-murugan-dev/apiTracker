3ae27493a4d9ac9468b69477b1d2b601709d13e0
Vignesh Murugan <vigmurug@gmail.com>
2025-06-15 13:32:44 +0530

    updated code of index

--- backend/api/index.py
+++ backend/api/index.py
@@ -1,4 +1,12 @@
-from backend.app import app
+# backend/api/index.py
 
-def handler(request, context):
-    return app(request.environ, start_response=lambda staus, headers: None)
+import sys
+import os
+
+# Ensure backend folder is on path
+sys.path.append(os.path.dirname(os.path.abspath(__file__)) + '/../')
+
+from app import app as flask_app  
+
+def handler(environ, start_response):
+    return flask_app(environ, start_response)
